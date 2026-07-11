da31f9e204f5c75d45b5d16daea8433df6d04ac1
geovar89 <geovar89@gmail.com>
2026-07-11 17:36:57 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ window.BIRTH_APP_CONFIG = {
 
   // Future use: Google Apps Script endpoint for pregnancy weight read/update.
   // WEIGHT_URL: "https://script.google.com/macros/s/XXXX/exec"
-,
   BIRTHHUB_API_URL: "https://script.google.com/macros/s/AKfycbxwT3q6lRpsO5EFg3RVNPp2ujIasaT_P9jVRczPldXYU_hj5JTd1hUH-Oql72vwKkys/exec",
   WEIGHT_SHEET_NAME: "Weight"
 ,
